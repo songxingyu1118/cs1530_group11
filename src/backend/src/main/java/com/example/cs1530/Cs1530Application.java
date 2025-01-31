1832fdee5f6b3318ea4b1b74805c2535aa7a7cb9
rucNick <ruc36@pitt.edu>
2025-01-31 18:04:06 -0500

    create src folder and move the front end files to the src

---
+++ src/backend/src/main/java/com/example/cs1530/Cs1530Application.java
@@ -0,0 +1,13 @@
+package com.example.cs1530;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs1530Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs1530Application.class, args);
+    }
+
+}
---
+++ src/backend/src/test/java/com/example/cs1530/Cs1530ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs1530;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs1530ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
