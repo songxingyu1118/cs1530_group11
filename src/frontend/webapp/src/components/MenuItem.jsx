4370d8f70a36c3aa2acd81f290d170c56445ece4
thegbars <gbarsotti262@gmail.com>
2025-03-05 19:21:44 -0500

    Menu item popup

--- src/frontend/webapp/src/components/MenuItem.jsx
+++ src/frontend/webapp/src/components/MenuItem.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+
 import {
     Card,
     CardContent,
     CardDescription,
     CardFooter
   } from "@/components/ui/card"
-  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog"
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons"
+
+import { LoremIpsum } from "@/components/LoremIpsum"
+
 
 function MenuItem({title, description, rating, price, image}) {
 
@@ -33,30 +48,63 @@ function MenuItem({title, description, rating, price, image}) {
 
     return (
         <div className="">
-            <Card>
-                <CardContent>
-                <CardDescription>
-                    <div className='flex flex-row gap-4'>
-                        <div class="flex-none w-1/2">
-                            <img src={image} alt={title} className='rounded-md' />
+
+            <Dialog>
+                <DialogTrigger>
+                    <Card>
+                        <CardContent>
+                        <CardDescription>
+                            <div className='flex flex-row gap-4'>
+                                <div class="flex-none w-1/2">
+                                    <img src={image} alt={title} className='rounded-md' />
+                                </div>
+                                
+                                <div className='flex flex-col justify-evenly'>
+                                    <h1 className='text-xl font-bold text-black'>{title}</h1>
+                                    <p className="line-clamp-3 md:line-clamp-4 lg:line-clamp-6 xl:line-clamp-8 text-neutral-400">{description}</p>
+                                </div>                        
+                            </div>
+                        </CardDescription>
+                        </CardContent>
+                        <CardFooter className="flex justify-between">
+                            <div className='flex gap-1'>
+                                {renderStars(rating)}
+                            </div>
+                            
+                            
+                            <div className="text-neutral-400">${price}</div>
+                        </CardFooter>
+                    </Card>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[725px]">
+                    <DialogHeader>
+                    <DialogTitle>
+                        <div className='flex flex-row gap-3 items-center'>
+                            {title}
+                            <div className='text-neutral-400 text-xs'>{price}</div>
                         </div>
+                    </DialogTitle>
+                    <DialogDescription>
+                        <ScrollArea className="h-48 my-2">
+                            <div>
+                                {description}
+                            </div>
+                        </ScrollArea>
+                    </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
                         
-                        <div className='flex flex-col'>
-                            <h1 className='text-xl font-bold text-black'>{title}</h1>
-                            <p className="line-clamp-3 md:line-clamp-4 lg:line-clamp-6 xl:line-clamp-8 text-neutral-400">{description}</p>
-                        </div>                        
                     </div>
-                </CardDescription>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    <div className='flex gap-1'>
-                        {renderStars(rating)}
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        
                     </div>
-                    
-                    
-                    <div className="text-neutral-400">${price}</div>
-                </CardFooter>
-            </Card>
+                    </div>
+                    <DialogFooter>
+                        
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
        </div>
     );
   }
