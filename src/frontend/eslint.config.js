8f1cc4dce0874aae13fe8bd903717fde699a2c10
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-03-11 22:14:32 +0000

    chore: Ignore errors in vite.config.js

--- src/frontend/eslint.config.js
+++ src/frontend/eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'vite.config.js'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -30,4 +30,4 @@ export default [
       ],
     },
   },
-]
+];
