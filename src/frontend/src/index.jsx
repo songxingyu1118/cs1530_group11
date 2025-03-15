f28bcf800ae109a543b26a319ba39f7901c34405
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-03-15 18:09:21 +0000

    fix: Use react-router-dom so the app is prod ready

--- src/frontend/src/index.jsx
+++ src/frontend/src/index.jsx
@@ -1,35 +1,65 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
-import './index.css';
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Navigate, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import HomePage from '@/pages/HomePage.jsx';
 import LoginPage from '@/pages/LoginPage.jsx';
 import RegisterPage from '@/pages/RegisterPage.jsx';
 import Cart from '@/pages/Cart.jsx';
+import FourOFour from '@/pages/FourOFour.jsx';
+
 import { Footer } from '@/components/Footer';
 import { Separator } from '@/components/ui/separator';
+import '@/index.css';
 
-const MainWrapper = ({ children }) => {
+const MainWrapper = () => {
   return (
-    <div className='p-4 container max-w-6xl mx-auto'>
-      {children}
-      <Separator className="my-8" />
-      <Footer />
-    </div>
+    <>
+      <div className='p-4 container max-w-6xl mx-auto'>
+        <Outlet />
+        <Separator className="my-8" />
+        <Footer />
+      </div>
+    </>
   );
 };
 
-createRoot(document.getElementById('root')).render(
-  <div>
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainWrapper><HomePage /></MainWrapper>} />
-          <Route path="/cart" element={<MainWrapper><Cart /></MainWrapper>} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  </div>
+const BASE_URL = import.meta.env.BASE_URL || '/';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainWrapper />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: 'cart', element: <Cart /> },
+      ],
+    },
+    {
+      path: '/login',
+      element: <LoginPage />,
+    },
+    {
+      path: '/register',
+      element: <RegisterPage />,
+    },
+    {
+      path: '/logout',
+      element: <Navigate to='/login' replace />,
+    },
+    {
+      path: '*',
+      element: <FourOFour />,
+    },
+  ],
+  {
+    basename: BASE_URL,
+  }
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
