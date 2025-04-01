8d22fda4d3df7b673be8afe5e78ad9fd3591366d
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-04-01 21:47:05 +0000

    fix: Correctly serialize dates

--- src/backend/src/main/java/com/example/cs1530/config/JacksonConfig.java
+++ src/backend/src/main/java/com/example/cs1530/config/JacksonConfig.java
@@ -2,7 +2,10 @@ package com.example.cs1530.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
@@ -12,6 +15,8 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
         return mapper;
     }
 }
